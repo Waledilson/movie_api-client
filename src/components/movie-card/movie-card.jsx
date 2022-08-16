e9b0dcf9ad8fa8775d3716af8b81e835b918c192
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-08-16 12:43:55 -0300

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 export class MovieCard extends React.Component {
     render() {
         const { movie } = this.props;
-        return <div className="movie-card" >{movie.Title}</div>;
+        return (
+            <div className="movie-view">
+                <div className="movie-poster">
+                    <img src={movie.ImagePath} />
+                </div>
+                <div className="movie-title">
+                    <span className="label">Title: </span>
+                    <span className="value">{movie.Title}</span>
+                </div>
+                <div className="movie-description">
+                    <span className="label">Description: </span>
+                    <span className="value">{movie.Description}</span>
+                </div>
+            </div>
+        );
     }
 }
