5714bfd51babe9b447e9c024deb03646641d90c9
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-08-17 23:43:47 -0300

    created main-view component
    added state
    added  props

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
 
@@ -11,23 +12,38 @@ export class MainView extends React.Component {
                 { _id: 1, Title: 'Inceptection', Description: 'Descrip....', ImagePath: '....' },
                 { _id: 2, Title: 'The Shawshank Redemption', Description: 'descrip....', ImagePath: '....' },
                 { _id: 3, Title: 'Gladiator', Description: 'descript....', ImagePath: '....' }
-            ]
-        }
+            ],
+            selectedMovie: null
+        };
+    }
+
+    setSelectedMovie(newSelectedMovie) {
+        this.setState({
+            selectedMovie: newSelectedMovie
+        });
     }
 
     render() {
-        const movies = this.state.movies;
-        if (movies.length === 0) {
+        const { movies, selectedMovie } = this.state;
+
+        if (movies.length === 0)
             return <div className="main-view">The list is empty!
             </div>;
-        } else {
-            return (
-                <div className="main-view">
-                    {movies.map(movie => <div key={movie._id}>{movie.Title}
-                    </div>)}
-                </div>
-            );
-        }
+
+        return (
+            <div className="main-view">
+                {selectedMovie
+                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => {
+                        this.setSelectedMovie(newSelectedMovie);
+                    }} />
+                    : movies.map(movie => (
+                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => {
+                            this.setSelectedMovie(movie)
+                        }} />
+                    ))
+                }
+            </div>
+        );
     }
 }
 export default MainView;
