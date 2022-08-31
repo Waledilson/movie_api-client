7c316043f16fadbc16c0c97b5cf1c74628b06e8c
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-08-31 09:30:08 -0300

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -16,14 +16,14 @@ export function LoginView(props) {
         <Form>
             <Form.Group controlId="formUsername">
                 <Form.Label>Username:</Form.Label>
-                <Form.Control type="text" onChange={e => setUsername(e.tartget.value)} />
+                <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
             </Form.Group>
 
             <Form.Group controlId="formPassword">
                 <Form.Label>Password:</Form.Label>
                 <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
             </Form.Group>
-            <Button variant="primary" type="submit" onClick={handSubmit}>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
                 Submit
             </Button>
         </Form>
