82f65e1c4b217b8c2871702d638d63d8c70499a2
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-01 22:21:33 -0300

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,7 @@
+import React, { useState } from 'react';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Form, Button } from 'react-bootstrap';
 
 export function RegistrationView(props) {
     const [username, setUsername] = useState('');
@@ -10,31 +12,41 @@ export function RegistrationView(props) {
     const handleRegistration = (e) => {
         e.preventDefault();
         console.log(username, password);
+        props.onRegistered(registered);
     };
     return (
-        <form>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <label>
-                Email:
-                <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
-            </label>
-            <label>
-                Birthday:
-                <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
-            </label>
-            <button type="submit" onClick={handleRegistration}>Register</button>
-        </form>
+        <Form>
+            <Form.Group>
+                <Form.Label>Username:</Form.Label>
+                <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} required placeholder="enter a username" />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Password:</Form.Label>
+                <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} required placeholder="enter a password" />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Email:</Form.Label>
+                <Form.Control type="text" value={email} onChange={e => setEmail(e.target.value)} required placeholder="enter your email" />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Birthday:</Form.Label>
+                <Form.Control type="date" value={birthday} onChange={e => setBirthday(e.target.value)} required placeholder="enter your date of birth" />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={handleRegistration}>Register</Button>
+        </Form >
     );
 }
+
 RegistrationView.propTypes = {
-    user: PropTypes.exact({
+    resgistered: PropTypes.exact({
+        Username: PropTypes.string.isRequired,
+        Password: PropTypes.string.isRequired,
+    }).isRequired
+
+}
+
+/*RegistrationView.propTypes = {
+    resgistered: PropTypes.exact({
         Username: PropTypes.string.isRequired,
         Password: PropTypes.string.isRequired,
         Email: PropTypes.string.isRequired,
@@ -42,3 +54,4 @@ RegistrationView.propTypes = {
     }).isRequired
 
 }
+*/
