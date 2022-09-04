512c2b859074630d97f0f468154ab00bd9655bf7
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-04 20:45:05 -0300

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Form, Button } from 'react-bootstrap';
 
 export function RegistrationView(props) {
