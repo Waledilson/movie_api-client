883884f5a0951fa0a224af613f523e0495b0e7f7
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-06 12:01:56 -0300

    added movie-view container
    added container containing most of the movie info to be displayed.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
@@ -7,22 +8,46 @@ export class MovieView extends React.Component {
         const { movie, onBackClick } = this.props;
 
         return (
-            <Row className="movie-view">
-                <Col className="movie-poster">
-                    <img src={movie.ImagePath} crossOrigin="true" />
-                </Col>
-                <Col className="movie-title">
-                    <span className="label">Title: </span>
-                    <span className="value">{movie.Title}</span>
-                </Col>
-                <Col className="movie-description">
-                    <span className="label">Description: </span>
-                    <span className="value">{movie.Description}</span>
-                </Col>
-                <button onClick={() => {
+            <Container className="movieViewContainer">
+                <Row className="movie-view">
+                    <Col className="movie-poster">
+                        <img width={250} height={350} src={movie.ImagePath} crossOrigin="true" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="movie-title">
+                        <span className="label">Title: </span>
+                        <span className="value">{movie.Title}</span>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="movie-description">
+                        <span className="label">Description: </span>
+                        <span className="value">{movie.Description}</span>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="movie-genre">
+                        <span className="label">Genre: </span>
+                        <span className="value">{movie.Genre.Name}</span>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="movie-director">
+                        <span className="label">Diracted by: </span>
+                        <span className="value">{movie.Director.Name}</span>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="movie-featured">
+                        <span className="label">Featured: </span>
+                        <span className="value">{movie.Featured}</span>
+                    </Col>
+                </Row>
+                <Button onClick={() => {
                     onBackClick(null);
-                }}>Back</button>
-            </Row>
+                }}>Back</Button>
+            </Container>
         );
     }
 }
