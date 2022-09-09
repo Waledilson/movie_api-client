cafa7586c29ee0fb60e3cc4ff8f4f95d64be1489
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-09 11:08:48 -0300

    added styling
    backgrounds - dark
    headline text - warning
    remaining text - white

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
+import './movie-view.scss';
+
 export class MovieView extends React.Component {
 
     render() {
         const { movie, onBackClick } = this.props;
 
         return (
-            <Container className="movieViewContainer" >
+            <Container>
                 <Row className="movie-view">
                     <Col className="movie-poster">
                         <img width={250} height={350} src={movie.ImagePath} crossOrigin="true" />
@@ -16,32 +18,32 @@ export class MovieView extends React.Component {
                 </Row>
                 <Row>
                     <Col className="movie-title">
-                        <span className="label">Title: </span>
-                        <span className="value">{movie.Title}</span>
+                        <span className="label text-warning">Title: </span>
+                        <span className="value text-white">{movie.Title}</span>
                     </Col>
                 </Row>
                 <Row>
                     <Col className="movie-description">
-                        <span className="label">Description: </span>
-                        <span className="value">{movie.Description}</span>
+                        <span className="label text-warning">Description: </span>
+                        <span className="value text-white">{movie.Description}</span>
                     </Col>
                 </Row>
                 <Row>
                     <Col className="movie-genre">
-                        <span className="label">Genre: </span>
-                        <span className="value">{movie.Genre.Name}</span>
+                        <span className="label text-warning">Genre: </span>
+                        <span className="value text-white">{movie.Genre.Name}</span>
                     </Col>
                 </Row>
                 <Row>
                     <Col className="movie-director">
-                        <span className="label">Diracted by: </span>
-                        <span className="value">{movie.Director.Name}</span>
+                        <span className="label text-warning">Directed by: </span>
+                        <span className="value text-white">{movie.Director.Name}</span>
                     </Col>
                 </Row>
                 <Row>
                     <Col className="movie-featured">
-                        <span className="label">Featured: </span>
-                        <span className="value">{movie.Featured}</span>
+                        <span className="label text-warning">Featured: </span>
+                        <span className="value text-white">{movie.Featured}</span>
                     </Col>
                 </Row>
                 <Button onClick={() => {
@@ -51,13 +53,13 @@ export class MovieView extends React.Component {
         );
     }
 }
-MovieView.PropTypes = {
+MovieView.propTypes = {
     movie: PropTypes.shape({
         ImagePath: PropTypes.string,
         Title: PropTypes.string.isRequired,
         Description: PropTypes.string,
-        Genre: PropTypes.string,
-        Director: PropTypes.string,
+        Genre: PropTypes.object,
+        Director: PropTypes.object,
         Featured: PropTypes.bool
     }),
     onBackClick: PropTypes.func.isRequired
