8e8085a04f9bcf224e920f020b99dd596ff42678
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-09 11:09:03 -0300

    added styling
    backgrounds - dark
    headline text - warning
    remaining text - white

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import React from 'react';
 import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
 
+import './registration-view.scss';
+
 export function RegistrationView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -19,23 +21,23 @@ export function RegistrationView(props) {
                 <Col sm={12} md={10} lg={8}>
                     <CardGroup>
                         <Card>
-                            <Card.Body>
-                                <Card.Header>Please Register</Card.Header>
+                            <Card.Body className="bg-dark text-white">
+                                <Card.Header className="text-warning">Please Register</Card.Header>
                                 <Form>
                                     <Form.Group>
-                                        <Form.Label>Username:</Form.Label>
+                                        <Form.Label className="text-warning" >Username:</Form.Label>
                                         <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} required placeholder="enter a username" />
                                     </Form.Group>
                                     <Form.Group>
-                                        <Form.Label>Password:</Form.Label>
+                                        <Form.Label className="text-warning">Password:</Form.Label>
                                         <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} required placeholder="your pass word must be at least 8 characters" />
                                     </Form.Group>
                                     <Form.Group>
-                                        <Form.Label>Email:</Form.Label>
+                                        <Form.Label className="text-warning">Email:</Form.Label>
                                         <Form.Control type="text" value={email} onChange={e => setEmail(e.target.value)} required placeholder="enter your email" />
                                     </Form.Group>
                                     <Form.Group>
-                                        <Form.Label>Birthday:</Form.Label>
+                                        <Form.Label className="text-warning">Birthday:</Form.Label>
                                         <Form.Control type="date" value={birthday} onChange={e => setBirthday(e.target.value)} required placeholder="enter your date of birth" />
                                     </Form.Group>
                                     <Button variant="primary" type="submit" onClick={handleRegistration}>Register</Button>
