dc23bdda74a803ea0e6d309d4561968aa48186a7
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-09 11:05:55 -0300

    updates
    added styling: backgrounds - dark, headline text - warning, other text- white
    
    started adding authentication code

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+
+import './login-view.scss';
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
@@ -10,18 +12,29 @@ export function LoginView(props) {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(username, password);
-        props.onLoggedIn(username);
+        /* Send a request to the server for authentication */
+        axios.post('https://intense-shore-03094.herokuapp.com/login', {
+            Username: username,
+            Password: password
+        })
+            .then(response => {
+                const data = response.data;
+                props.onLoggedIn(data);
+            })
+            .catch(e => {
+                console.log('no such user')
+            });
     };
 
     return (
-        <Form>
+        <Form className="bg-dark text-white">
             <Form.Group controlId="formUsername">
-                <Form.Label>Username:</Form.Label>
+                <Form.Label className="text-warning">Username:</Form.Label>
                 <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
             </Form.Group>
 
             <Form.Group controlId="formPassword">
-                <Form.Label>Password:</Form.Label>
+                <Form.Label className="text-warning">Password:</Form.Label>
                 <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
             </Form.Group>
             <Button variant="primary" type="submit" onClick={handleSubmit}>
