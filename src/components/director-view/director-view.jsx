098d1bf87b92db07dfd8032aeae144b71dbb9da0
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-14 16:33:09 -0300

    Update director-view.jsx
    added card with director name, bio, birthyear

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import PropTypes from 'prop-types'
+import { Card, Container, Button } from 'react-bootstrap';
 
 export class DirectorView extends React.Component {
     render() {
         const { director, onBackClick } = this.props;
 
         return (
-            <Card>
-                <Card.Title>{movie.director.name}</Card.Title>
-            </Card>
-        )
+            <Container>
+                <Card className="director-view">
+                    <Card.Header>Director</Card.Header>
+                    <Card.Body>{director.Name}</Card.Body>
+                    <Card.Body>Born: {director.Birth}</Card.Body>
+                    <Card.Body>{director.Bio}</Card.Body>
+                    <Card.Footer>
+                        <Button onClick={() => {
+                            onBackClick(null);
+                        }}>Back</Button>
+                    </Card.Footer>
+                </Card>
+            </Container>
+        );
     }
 
 }
+DirectorView.propTypes = {
+    Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Bio: PropTypes.string,
+        Birth: PropTypes.number
+    }).isRequired
+};
