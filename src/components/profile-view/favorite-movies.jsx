06870e560d155c4740aabdf2dfcc9fa73ce4e22b
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-14 16:35:21 -0300

    updated
    still unsure about this

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
 import Link from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap';
 
-function FavoriteMovies({ favoriteMovieList }) {
+function FavoriteMovies({ favoriteMovieList, removeFav }) {
     return (
-        <div>
-            <h2>Favorite Movies</h2>
-            {favoriteMovieList.map((movies) => {
-                return (
-                    <div key={movies._id}>
-                        <img src={movies.ImagePath} />
-                        <Link to={`/movies/${movies._id}`}>
-                            <h4>{movies.Title}</h4>
-                        </Link>
-                        <button variant="secondary" onClick={() => removeFav(movies._id)}>Remove from list</button>
-                    </div>
-                )
-            })
-            }
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    <h2>Favorite Movies</h2>
+                </Col>
+                <Row>
+                    {favoriteMovieList.map((movies) => {
+                        return (
+                            <Col className="fav-movie-list" xs={12} sm={6} lg={3} key={movies._id}>
+                                <img src={movies.ImagePath} />
+                                <Link to={`/movies/${movies._id}`}>
+                                    <h4>{movies.Title}</h4>
+                                </Link>
+                                <button variant="secondary" onClick={() => removeFav(movies._id)}>Remove from favorites list</button>
+                            </Col>
+                        )
+                    })
+                    }
+                </Row>
+            </Row>
+        </Container>
+
     )
 }
 
