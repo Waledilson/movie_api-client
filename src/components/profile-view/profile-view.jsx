df37a4d637080f4bdc26dbcdfcf2545d916a7f50
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-14 16:35:52 -0300

    work in progress?
    not sure exactly what to do with this yet

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,50 +1,82 @@
+import React from 'react';
+import React, { useState } from 'react';
+import { Card, Container, Col, Row } from 'react-bootstrap';
+
 import UserInfo from './user-info';
 import FavoriteMovies from './favorite-movies';
-import { useEffect } from "react";
 import UpdateUser from './update-user';
 
 import './profile-view.scss';
 
 
-export function ProfileView({ movies, onUpdatedUserInfo }) {
+export function ProfileView({ movies }) {
+
     const [user, setUser] = useState({
-    })
-
-    const favoriteMovieList = movies.filter((mosies) => {
-
+        user: localStorage.getItem('data')
     });
 
-    const getUser = () => {
-
-    }
-
-    const handleSubmit = (e) => {
-
-    }
-    const removeFav = (id) => {
-
-    }
-
-    const handleUpdate = (e) => {
-
+    getUser = (token) => {
+        const Username = localStorage.getItem("user");
+        axios
+            .get(`https://ap-myflix.herokuapp.com/users/${Username}`, {
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            .then((response) => {
+                this.setState({
+                    Username: response.data.Username,
+                    Password: response.data.Password,
+                    Email: response.data.Email,
+                    Birthday: response.data.Birthday,
+                    FavoriteMovies: response.data.FavoriteMovies,
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     };
 
-    useEffect(() => {
-    }, [])
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isReq = validate();
+        if (isReq) {
+            /* Send a request to the server for authentication */
+            axios.put('https://intense-shore-03094.herokuapp.com/user', {
+                Username: username,
+                Password: password,
 
-    return (
-        <Container>
-            <Row>
-                <Col xs={12} sm={4}>
-                    <UserInfo name={user.Username} email={user.Email} />
-                </Col>
-                <Col xs={12} sm={8}>
-                    <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
-                </Col>
-            </Row>
+            })
+                .then(response => {
+                    const data = response.data;
+                    props.onLoggedIn(data);
+                })
+                .catch(e => {
+                    console.log('no such user')
+                });
+        }
 
-            <FavoriteMovies favoriteMovieList={favoriteMovieList} />
+        return (
+            <Container>
+                <Row>
+                    <Col xs={12} sm={4}>
+                        <Card>
+                            <Card.Body>
+                                <UserInfo name={user.Username} email={user.Email} />
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={12} sm={8}>
+                        <Card>
+                            <Card.Body>
+                                <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
 
-        </Container>
-    );
+                <FavoriteMovies favoriteMovieList={favoriteMovieList} />
+
+
+            </Container>
+        );
+    };
 }
