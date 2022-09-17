53ababb37cd8e51fbba4c23ac170a6a5e3e802a1
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-16 21:14:14 -0300

    Update profile-view.jsx
    trying to make it work.
    changed "onFavoriteMovie" to "handleFavorite".
    handleFavorite SHOULD handle both adding and removing favs from fav list.

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import { Card, Container, Col, Row } from 'react-bootstrap';
 
 import UserInfo from './user-info';
@@ -7,52 +7,87 @@ import FavoriteMovies from './favorite-movies';
 import UpdateUser from './update-user';
 
 import './profile-view.scss';
+import axios from 'axios';
 
 
-export function ProfileView({ movies }) {
-
-    const [user, setUser] = useState({
-        user: localStorage.getItem('data')
-    });
-
+export class ProfileView extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [],
+            Username: null,
+            Password: null,
+            Email: null,
+            Birthday: null,
+            FavoriteMovies: []
+        }
+    }
+    componentDidMount() {
+        let accessToken = localStorage.getItem('token');
+        if (accessToken !== null) {
+            this.setState({
+                user: localStorage.getItem('user')
+            });
+            this.getUser(accessToken);
+        }
+    }
     getUser = (token) => {
-        const Username = localStorage.getItem("user");
-        axios
-            .get(`https://ap-myflix.herokuapp.com/users/${Username}`, {
-                headers: { Authorization: `Bearer ${token}` },
-            })
+        const Username = localStorage.getItem('user')
+        axios.get('https://intense-shore-03094.herokuapp.com/users/${username}', {
+            headers: { Authorization: `Bearer ${token}` },
+        })
             .then((response) => {
                 this.setState({
-                    Username: response.data.Username,
-                    Password: response.data.Password,
-                    Email: response.data.Email,
-                    Birthday: response.data.Birthday,
-                    FavoriteMovies: response.data.FavoriteMovies,
+                    Username: response.data.username,
+                    Password: response.data.password,
+                    Email: response.data.email,
+                    Birthday: response.data.birthday,
+                    FavoriteMovies: response.data.favoriteMovies
                 });
             })
             .catch(function (error) {
                 console.log(error);
             });
-    };
+    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const isReq = validate();
-        if (isReq) {
-            /* Send a request to the server for authentication */
-            axios.put('https://intense-shore-03094.herokuapp.com/user', {
-                Username: username,
-                Password: password,
-
-            })
-                .then(response => {
-                    const data = response.data;
-                    props.onLoggedIn(data);
+    handleFavorite = (movieId, action) => {
+        const { Username, favoriteMovies } = this.state;
+        const username = localStorage.getItem('user');
+        const token = localStorage.getItem('token');
+        if (!user) {
+            if (action === 'add') {
+                this.setState({ favoriteMovies, movieId })
+                axios.post('https://intense-shore-03094.herokuapp.com/users/${username}/favoriteMovies', {
+                    headers: { Authroization: `Bearer ${token}` }
                 })
-                .catch(e => {
-                    console.log('no such user')
-                });
-        }
+                    .then((response) => {
+                        console.log(response);
+                        alert('${movie.Name} has ben added to ${Username}\'s favorite movie list!');
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
+            } else if (action === 'remove') {
+                this.setState({ username, favoriteMovies })
+                axios.delete('https://intense-shore-03094.herokuapp.com/users/${username}/favoriteMovies', {
+                    headers: { Authroization: `Bearer ${token}` }
+                })
+                    .then((response) => {
+                        console.log(response);
+                        alert('${movie.Name} has been removed from ${Username}\'s favorite movie list!');
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+        };
+    }
+
+
+
+
+    render() {
+        const { handleUpdate, handleFavorite, Username, Email } = this.state;
 
         return (
             <Container>
@@ -60,23 +95,24 @@ export function ProfileView({ movies }) {
                     <Col xs={12} sm={4}>
                         <Card>
                             <Card.Body>
-                                <UserInfo name={user.Username} email={user.Email} />
+                                <UserInfo name={Username} email={Email} />
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col xs={12} sm={8}>
                         <Card>
                             <Card.Body>
-                                <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
+                                <UpdateUser handleUpdate={handleUpdate} />
                             </Card.Body>
                         </Card>
                     </Col>
+                    <Col>
+                        <FavoriteMovies handleFavorite={handleFavorite} />
+                    </Col>
                 </Row>
-
-                <FavoriteMovies favoriteMovieList={favoriteMovieList} />
-
-
-            </Container>
+            </Container >
         );
-    };
-}
+    }
+};
+
+
