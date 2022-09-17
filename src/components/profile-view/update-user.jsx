74206948d639b29ac592e29b9ccaf1548199845b
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-16 21:20:55 -0300

    Update update-user.jsx
    added handleUpdate const to update user data.

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 
-function UpdateUser(handleSubmit, handleUpdate) {
+export function UpdateUser(user) {
+    const username = localStorage.getItem('username');
+    const password = localStorage.getItem('password');
+    const email = localStorage.getItem('email');
+    const birthday = localStorage.getItem('birthday');
 
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        axios.put('https://intense-shore-03094.herokuapp.com/login', {
+            Username: username,
+            Password: password,
+            Email: email,
+            Birthday: birthday
+        })
+            .then(response => {
+                const data = response.data;
+                console.log(data);
+                //window.open('/', '_self');
+            })
+            .catch(e => {
+                console.log('error updating your info')
+            });
+    };
 
     return (
-        <form className='profile-form' onSubmit={(e) => handleSubmit(e)}>
+        <form className='profile-form' onSubmit={(e) => handleUpdate(e)}>
             <h2>Want to change some info??</h2>
             <label>Username:</label>
             <input type='text' name='Username' defaultValue={user.Username} onChange={e => handleUpdate(e)} />
             <label>Password</label>
-            <input type='password' name='password' defaultValue={user.Password} onChange={e => handleSubmit(e)} />
+            <input type='password' name='password' defaultValue={user.Password} onChange={e => handleUpdate(e)} />
             <label>Email address</label>
             <input type='email' name='email' defaultValue={user.Email} onChange={e => handleUpdate(e.target.value)} />
             <button variant='primary' type='submit'>Update</button>
