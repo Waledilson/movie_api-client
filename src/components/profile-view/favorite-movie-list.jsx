62b299de47324ad41958fdf0241f698e64bc50eb
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-18 17:20:28 -0300

    Create favorite-movie-list.jsx
    renamed favoriteMovies to favoriteMovieList

---
+++ src/components/profile-view/favorite-movie-list.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+// import Link from "react-router-dom";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+export class FavoriteMovieList extends React.Component {
+
+    render() {
+        const { handleFavorite, favoriteMovies } = this.props;
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Favorite Movies</h2>
+                    </Col>
+                    <Col>
+                        {favoriteMovies.map((movie) => {
+                            return (
+                                <Col key={_id}>
+                                    <img src={ImagePath} />
+                                    <Col>{Title}</Col>
+                                    <Button variant="danger" onClick={() => handleFavorite(_id, 'remove')}>remove from favorites</Button>
+                                </Col>
+                            )
+                        })}
+
+
+                    </Col>
+                </Row>
+            </Container>
+
+        );
+    }
+};
+
