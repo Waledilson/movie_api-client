b572ddf8796916684482a411bc3bf240a3917d0a
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-18 17:20:58 -0300

    Update update-user.jsx

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-export function UpdateUser(user) {
+export default UpdateUser = (user) => {
     const username = localStorage.getItem('username');
     const password = localStorage.getItem('password');
     const email = localStorage.getItem('email');
     const birthday = localStorage.getItem('birthday');
 
+
     const handleUpdate = (e) => {
         e.preventDefault();
-        axios.put('https://intense-shore-03094.herokuapp.com/login', {
+        axios.put(`https://intense-shore-03094.herokuapp.com/users/:username`, {
             Username: username,
             Password: password,
             Email: email,
@@ -17,26 +18,28 @@ export function UpdateUser(user) {
             .then(response => {
                 const data = response.data;
                 console.log(data);
-                //window.open('/', '_self');
             })
-            .catch(e => {
+            .catch((e) => {
                 console.log('error updating your info')
             });
     };
 
     return (
-        <form className='profile-form' onSubmit={(e) => handleUpdate(e)}>
+        <form className='profile-form' onSubmit={(e) => handleSubmit(e)}>
             <h2>Want to change some info??</h2>
             <label>Username:</label>
-            <input type='text' name='Username' defaultValue={user.Username} onChange={e => handleUpdate(e)} />
+            <input type='text' name='Username' defaultValue={user.Username} onChange={e => handleUpdate(e.target.value)} />
             <label>Password</label>
-            <input type='password' name='password' defaultValue={user.Password} onChange={e => handleUpdate(e)} />
+            <input type='password' name='password' defaultValue={user.Password} onChange={e => handleUpdate(e.target.value)} />
             <label>Email address</label>
             <input type='email' name='email' defaultValue={user.Email} onChange={e => handleUpdate(e.target.value)} />
             <button variant='primary' type='submit'>Update</button>
+            <label>Birthday</label>
+            <input type='date' name='birthday' defaultValue={user.Birthday} onChange={e => handleUpdate(e.target.value)} />
+
 
         </form>
     )
-}
+};
 
-export default UpdateUser
+
