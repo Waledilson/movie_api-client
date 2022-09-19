ec62d4c8197d3c375f59646973f6efcfd9b77f05
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-19 16:08:09 -0300

    Update genre-view.jsx
    started genreView skeleton code

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,2 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import { Card, Container, Button } from 'react-bootstrap';
+
+export class GenreView extends React.Component {
+
+
+    render() {
+        const { genre, onBackClick } = this.props;
+
+
+        return (
+            <Container>
+                <Card className="genre-view">
+                    <Card.Header>genre</Card.Header>
+                    <Card.Body>{genre.Name}</Card.Body>
+                    <Card.Body>Description: {genre.description}</Card.Body>
+                    <Card.Body>{ }</Card.Body>
+                    <Card.Footer>
+                        <Button onClick={() => {
+                            onBackClick(null);
+                        }}>Back</Button>
+                    </Card.Footer>
+                </Card>
+            </Container>
+        );
+    }
+
+}
+GenreView.propTypes = {
+    Genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Description: PropTypes.string
+    }).isRequired
+};
 
