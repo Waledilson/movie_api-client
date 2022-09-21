90cf77c17186a7c6081d54650a7e11592a8fedcd
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-21 19:45:30 -0300

    Update movie-card.jsx
    turned component into function component.
    using addFavorite to button.
    fixed props {movie, addFavorite}

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -7,25 +7,24 @@ import { Link } from "react-router-dom";
 
 import './movie-card.scss';
 
-export class MovieCard extends React.Component {
-    render() {
-        const { movie, handleFavorite, favoriteMovies } = this.props;
+export const MovieCard = (props) => {
+    const { movie, addFavorite } = props;
 
-        return (
-            <Card className="bg-dark">
-                <Card.Img crossOrigin="true" variant="top" src={props.movie.ImagePath} />
-                <Card.Body>
-                    <Card.Title className="text-warning">{props.movie.Title}</Card.Title>
-                    <Card.Text className="text-white">{props.movie.Description}</Card.Text>
-                    <Link to={`/movies/${movie._id}`}>
-                        <Button variant="link">Open</Button>
-                    </Link>
-                    <Button variant="danger" onClick={() => { handleFavorite(favoriteMovies, 'add') }}>add to favorites</Button>
-                </Card.Body>
-            </Card>
-        );
-    }
+    return (
+        <Card className="bg-dark">
+            <Card.Img crossOrigin="true" variant="top" src={props.movie.ImagePath} />
+            <Card.Body>
+                <Card.Title className="text-warning">{props.movie.Title}</Card.Title>
+                <Card.Text className="text-white">{props.movie.Description}</Card.Text>
+                <Link to={`/movies/${movie._id}`}>
+                    <Button variant="link">Open</Button>
+                </Link>
+                <Button variant="danger" onClick={() => { addFavorite(movie) }}>add to favorites</Button>
+            </Card.Body>
+        </Card>
+    );
 }
+
 
 MovieCard.propTypes = {
     movie: PropTypes.shape({
