67f57ea259a2c4478bd497685745b231b0d0a157
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-21 19:46:21 -0300

    Update favorite-movie-list.jsx
    using delFavorite in button to delete favorite movie from user's list of favorites

--- src/components/profile-view/favorite-movie-list.jsx
+++ src/components/profile-view/favorite-movie-list.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 // import Link from "react-router-dom";
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
-export function FavoriteMovieList({ favoriteMovies, handleFavorite }) {
+export function FavoriteMovieList({ favoriteMovies, delFavorite }) {
     return (
 
         <Container>
@@ -13,10 +13,10 @@ export function FavoriteMovieList({ favoriteMovies, handleFavorite }) {
                 <Col>
                     {favoriteMovies.map((movie) => {
                         return (
-                            <Col key={props.movie._id}>
-                                <img crossOrigin="true" src={props.movie.ImagePath} />
-                                <Col>{props.movie.Title}</Col>
-                                <Button variant="danger" onClick={() => { handleFavorite(_id, 'remove') }}>remove from favorites</Button>
+                            <Col key={movie._id}>
+                                <img crossOrigin="true" src={movie.ImagePath} />
+                                <Col>{movie.Title}</Col>
+                                <Button variant="danger" onClick={() => { delFavorite(movie) }}>remove from favorites</Button>
                             </Col>
                         )
                     })
