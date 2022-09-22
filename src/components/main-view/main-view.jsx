ef19b1832122648cf6d83e540ba69b7fe81f2904
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-09-21 21:28:23 -0300

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -60,23 +60,23 @@ export class MainView extends React.Component {
         this.getMovies(authData.token);
     }
 
-    addFavorite = (user, movie) => {
-        axios.post(`https://intense-shore-03094.herokuapp.com/users/:Username/movies/`, {
-            headers: { Authorization: `Bearer ${token}` }
-        })
-            .then((response) => {
-                this.setState({ user, movie })
-                console.log(response)
-                alert(`${movie} has been added to ${user}\'s favorite movie list!`)
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
+    // addFavorite = (user, movie) => {
+    //     e.preventDefault();
+    //     axios.post(`https://intense-shore-03094.herokuapp.com/users/:Username/movies/`, {
+    //         headers: { Authorization: `Bearer ${token}` }
+    //     })
+    //         .then((response) => {
+    //             this.setState({ user, movie });
+    //             console.log(response);
+    //             alert(`${movie} has been added to ${user}\'s favorite movie list!`);
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         })
+    // }
 
     render() {
-        const { movies, user, addFavorite } = this.state;
-        // const { handleFavorite } = this.props;
+        const { movies, user } = this.state;
 
         if (!user) return <Row>
             <Col>
