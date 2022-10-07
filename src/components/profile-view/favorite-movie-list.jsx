fabbdc800826ab73b46e4e00554b72a929528fea
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-10-07 16:19:02 -0300

    Update favorite-movie-list.jsx

--- src/components/profile-view/favorite-movie-list.jsx
+++ src/components/profile-view/favorite-movie-list.jsx
@@ -2,34 +2,32 @@ import React from 'react'
 // import Link from "react-router-dom";
 import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 
-export function FavoriteMovieList({ favoriteMovies, delFavorite }) {
-    const { movie } = movie.props;
-    return (
+export function FavoriteMovieList({ favoriteMovies, delFavorite, movie, ImagePath, Title }) {
 
-        <Container>
+    return (
+        < Container >
             <Row>
+
                 <Col>
-                    <h2>Favorite Movies</h2>
-                </Col>
-                <Col>
-                    {favoriteMovies.map((movie) => {
-                        return (
-                            <Card key={movie._id}>
-                                <Card.Img crossOrigin="true" src={movie.ImagePath} />
-                                <Card.Body>
-                                    <Card.Title>{movie.Title}</Card.Title>
-                                    <Button variant="danger" onClick={() => { delFavorite(movie) }}><Card.Text>remove from favorites</Card.Text></Button>
-                                </Card.Body>
-                            </Card>
-                        )
-                    })
-                    }
+                    <Col>
+                        <Card>
+                            <Card.Img crossOrigin="true" src={movie.ImagePath} />
+                            <Card.Body>
+                                <Card.Title >{movie.Title}</Card.Title>
+                                <Button variant="danger" onClick={() => { delFavorite(movie) }}>remove from favorites</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
 
                 </Col>
             </Row>
-        </Container>
+        </Container >
 
     );
+
+    // function newFunction() {
+    //     return movie.props;
+    // }
 }
 
 
