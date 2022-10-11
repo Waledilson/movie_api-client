b0235aff8302c88538cc0a8f03a8780a204fcbd4
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-10-11 19:47:50 -0300

    Update favorite-movie-list.jsx
    got rid of unused props and unused <Col>

--- src/components/profile-view/favorite-movie-list.jsx
+++ src/components/profile-view/favorite-movie-list.jsx
@@ -1,33 +1,24 @@
 import React from 'react'
 // import Link from "react-router-dom";
-import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import { Container, Col, Row, Button, Card } from 'react-bootstrap';
 
-export function FavoriteMovieList({ favoriteMovies, delFavorite, movie, ImagePath, Title }) {
-
+export function FavoriteMovieList({ movie, delFavorite }) {
     return (
-        < Container >
+        < Container>
             <Row>
-
                 <Col>
-                    <Col>
-                        <Card>
-                            <Card.Img crossOrigin="true" src={movie.ImagePath} />
-                            <Card.Body>
-                                <Card.Title >{movie.Title}</Card.Title>
-                                <Button variant="danger" onClick={() => { delFavorite(movie) }}>remove from favorites</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-
+                    <Card>
+                        <Card.Img crossOrigin="true" src={movie.ImagePath} />
+                        <Card.Body>
+                            <Card.Title >{movie.Title}</Card.Title>
+                            <Button variant="danger" onClick={() => { delFavorite(movie) }}>remove from favorites</Button>
+                        </Card.Body>
+                    </Card>
                 </Col>
             </Row>
         </Container >
 
     );
-
-    // function newFunction() {
-    //     return movie.props;
-    // }
 }
 
 
