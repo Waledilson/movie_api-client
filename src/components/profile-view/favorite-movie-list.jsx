4b20899c97c50c49515a2247069b9dd04d0c4d81
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-10-12 21:13:28 -0300

    Update favorite-movie-list.jsx
    started adding bootstrap

--- src/components/profile-view/favorite-movie-list.jsx
+++ src/components/profile-view/favorite-movie-list.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 // import Link from "react-router-dom";
-import { Container, Col, Row, Button, Card } from 'react-bootstrap';
+import { Container, Col, Row, Button, Card, } from 'react-bootstrap';
 
 export function FavoriteMovieList({ movie, delFavorite }) {
     return (
-        < Container>
-            <Row>
-                <Col>
+        < Container >
+            <Row xs={12} md={6} lg={3}>
+                <Col xs={12} md={6} lg={3} >
                     <Card>
-                        <Card.Img crossOrigin="true" src={movie.ImagePath} />
+                        <Card.Img className='movie-poster' crossOrigin="true" src={movie.ImagePath} />
                         <Card.Body>
                             <Card.Title >{movie.Title}</Card.Title>
                             <Button variant="danger" onClick={() => { delFavorite(movie) }}>remove from favorites</Button>
