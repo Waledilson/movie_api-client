5962946cd58618702e88fa11e8ce6d539a0c289f
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-10-16 22:46:12 -0300

    restructured card
    restructured card to add html <p> and <span> elements

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -8,14 +8,22 @@ export class GenreView extends React.Component {
     render() {
         const { genre, onBackClick } = this.props;
 
-
         return (
             <Container>
-                <Card className="genre-view">
-                    <Card.Header>Genre:</Card.Header>
-                    <Card.Body>{genre.Name}</Card.Body>
-                    <Card.Body>Description: {genre.description}</Card.Body>
-                    <Card.Body>{ }</Card.Body>
+                <Card className="genre-view bg-dark">
+                    <Card.Header>
+                        <Card.Title className="text-warning">Genre</Card.Title>
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            <p className="text-warning">Genre Name:
+                                <span className="text-white"> {genre.Name}</span></p>
+                        </Card.Text>
+                        <Card.Text>
+                            <p className="text-warning">Description:
+                                <span className="text-white"> {genre.Description}</span></p>
+                        </Card.Text>
+                    </Card.Body>
                     <Card.Footer>
                         <Button onClick={() => {
                             onBackClick(null);
@@ -33,4 +41,6 @@ GenreView.propTypes = {
         Description: PropTypes.string
     })
 };
+
+
 
