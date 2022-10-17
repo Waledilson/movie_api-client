03a81f2ad57b0894f94b2218a790616f3eaf6599
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-10-16 22:45:35 -0300

    restructured card
    restructured card to add html <p> and <span> elements

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -7,12 +7,25 @@ export class DirectorView extends React.Component {
         const { director, onBackClick } = this.props;
 
         return (
-            <Container>
-                <Card className="director-view">
-                    <Card.Header>Director</Card.Header>
-                    <Card.Body>{director.Name}</Card.Body>
-                    <Card.Body>Born: {director.Birth}</Card.Body>
-                    <Card.Body>{director.Bio}</Card.Body>
+            <Container className="bg-dark">
+                <Card className="director-view bg-dark">
+                    <Card.Header>
+                        <Card.Title className="text-warning">Director</Card.Title>
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            <p className="text-warning">Name:
+                                <span className="text-white"> {director.Name}</span></p>
+                        </Card.Text>
+                        <Card.Text>
+                            <p className="text-warning">Born:
+                                <span className="text-white"> {director.Birth}</span></p>
+                        </Card.Text>
+                        <Card.Text>
+                            <p className="text-warning">Bio:
+                                <span className="text-white"> {director.Bio}</span></p>
+                        </Card.Text>
+                    </Card.Body>
                     <Card.Footer>
                         <Button onClick={() => {
                             onBackClick(null);
