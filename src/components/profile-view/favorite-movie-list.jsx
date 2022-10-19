8181235522596a650309a37aaa443b0b4de74d24
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-10-19 13:42:27 -0300

    Update favorite-movie-list.jsx

--- src/components/profile-view/favorite-movie-list.jsx
+++ src/components/profile-view/favorite-movie-list.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 // import Link from "react-router-dom";
 import { Container, Col, Row, Button, Card } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 export function FavoriteMovieList({ movie, delFavorite }) {
     return (
 
-        <Card xs={6} md={6} lg={3}>
-            <Card.Img xs={6} md={3} className='movie-poster' crossOrigin="true" src={movie.ImagePath} />
+        <Card className="flex-fill" xs={6} md={6} lg={3}>
             <Card.Body className="bg-dark text-warning">
-                <Card.Title >{movie.Title}</Card.Title>
-                <Button variant="danger" size="sm" onClick={() => { delFavorite(movie) }}>remove from favorites</Button>
+                <Link to={`/movies/${movie._id}`}>
+                    <Card.Img xs={6} md={3} crossOrigin="true" src={movie.ImagePath} />
+                </Link>
+                <Button text="black" variant="warning" size="sm" onClick={() => { delFavorite(movie) }}>remove from favorites</Button>
             </Card.Body>
         </Card>
     );
