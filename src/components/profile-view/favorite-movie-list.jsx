3c3e55de3e559207e979d51f53b5a2b63aa9cdb3
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-10-23 20:51:04 -0300

    bootstrap

--- src/components/profile-view/favorite-movie-list.jsx
+++ src/components/profile-view/favorite-movie-list.jsx
@@ -6,12 +6,12 @@ import { Link } from "react-router-dom";
 export function FavoriteMovieList({ movie, delFavorite }) {
     return (
 
-        <Card className="flex-fill" xs={6} md={6} lg={3}>
-            <Card.Body className="bg-dark text-warning">
+        <Card className="flex-fill bg-dark text-warning" xs={6} md={6} lg={3}>
+            <Card.Body>
                 <Link to={`/movies/${movie._id}`}>
                     <Card.Img xs={6} md={3} crossOrigin="true" src={movie.ImagePath} />
                 </Link>
-                <Button text="black" variant="warning" size="sm" onClick={() => { delFavorite(movie) }}>remove from favorites</Button>
+                <Button variant="dark text-primary" size="sm" onClick={() => { delFavorite(movie) }}>remove from favorites</Button>
             </Card.Body>
         </Card>
     );
