2b2eed229faa94dd93d04ca8d13cdc591b942c21
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-10-25 13:31:30 -0300

    Update nav-bar.jsx

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Container, Button } from 'react-bootstrap';
-
 
 export function Navbar(props) {
     const { user } = props;
@@ -18,13 +16,11 @@ export function Navbar(props) {
     return (
 
         <Navbar fixed="top" bg="dark" variant="dark">
-            <Container>
-                <Nav className="me-auto">
-                    <Navbar.Brand href={`/`} className="text-warning float-left">Martini Shot</Navbar.Brand>
-                    <Nav.Link className="text-primary" href={`/users/${user}`}>Profile</Nav.Link>
-                    <Nav.Link size="sm" variant="link" className="float-right text-primary" onClick={(handleLogOut)}>Log out</Nav.Link>
-                </Nav>
-            </Container>
+            <Navbar.Brand href={`/`} className="text-warning">Martini Shot</Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link className="text-primary" href={`/users/${user}`}>Profile</Nav.Link>
+                <Nav.Link size="sm" variant="link" className="float-right text-primary" onClick={(handleLogOut)}>Log out</Nav.Link>
+            </Nav>
         </Navbar>
     );
 }
