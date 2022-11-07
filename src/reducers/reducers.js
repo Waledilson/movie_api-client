7a11bca99fc5f9542e9c06e929973c3730b55aca
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-11-07 13:40:39 -0400

    Update reducers.js
    added user reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { SET_FILTER, SET_MOVIES } from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER, CREATE_USER, LOGIN_USER, UPDATE_USER, USER_FAV, ADD_FAV, REMOVE_FAV, DELETE_USER } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
     switch (action.type) {
@@ -19,9 +19,46 @@ function movies(state = [], action) {
     }
 }
 
+function user(state = {}, action) {
+    switch (action.type) {
+        case SET_USER:
+            return action.value;
+        case CREATE_USER:
+            return action.value;
+        case LOGIN_USER:
+            return action.value;
+        case UPDATE_USER:
+            return action.value;
+        case USER_FAV:
+            return action.value;
+        case ADD_FAV:
+            return {
+                ...state,
+                FavoriteMovies: [
+                    ...state?.FavoriteMovies,
+                    action.value
+                ]
+            }
+        case REMOVE_FAV:
+            return {
+                ...state,
+                FavoriteMovies: [
+                    ...state?.FavoriteMovies,
+                    action.value
+                ]
+            }
+        case DELETE_USER:
+            return action.value;
+
+        default:
+            return state;
+    }
+}
+
 const moviesApp = combineReducers({
     visibilityFilter,
-    movies
+    movies,
+    user
 });
 
 export default moviesApp;
