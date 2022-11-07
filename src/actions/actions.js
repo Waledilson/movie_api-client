fa29d2f5984dd6ca890412ee21a1773a6ead1e3f
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-11-07 13:39:32 -0400

    Update actions.js
    added user actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,13 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_USER = 'SET USER';
+export const CREATE_USER = 'CREATE USER';
+export const LOGIN_USER = 'LOGIN USER';
+export const UPDATE_USER = 'UPDATE USER';
+export const USER_FAV = 'USER FAV';
+export const ADD_FAV = 'ADD FAV';
+export const REMOVE_FAV = 'REMOVE FAV';
+export const DELETE_USER = 'DELETE USER';
 
 export function setMovies(value) {
     return {
@@ -13,5 +21,61 @@ export function setFilter(value) {
         type: SET_FILTER,
         value
     };
+}
+
+export function setUser(value) {
+    return {
+        type: SET_USER,
+        value
+    };
+}
+
+export function createUser(value) {
+    return {
+        type: CREATE_USER,
+        value
+    }
+}
+
+export function loginUser(value) {
+    return {
+        type: LOGIN_USER,
+        value
+    }
+}
+
+export function updateUser(value) {
+    return {
+        type: UPDATE_USER,
+        value
+    }
+}
+
+export function userFav(value) {
+    return {
+        type: USER_FAV,
+        value
+    }
+}
+
+export function addFav(value) {
+    return {
+        type: ADD_FAV,
+        value
+    }
+}
+
+export function removeFav(value) {
+    return {
+        type: REMOVE_FAV,
+        value
+    }
+}
+
+export function deleteUser(value) {
+    return {
+        type: DELETE_USER,
+        value
+    }
 }
 
