222a3f0b91aa11b53391ec78cfd79fb712701153
Waledilson <95579186+Waledilson@users.noreply.github.com>
2022-11-25 22:11:12 -0400

    moved delFavorite function
    moved delFavorite function as this is where it will be called. not working yet

--- src/components/profile-view/favorite-movie-list.jsx
+++ src/components/profile-view/favorite-movie-list.jsx
@@ -2,8 +2,36 @@ import React from "react";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import axios from "axios";
 
-export function FavoriteMovieList({ movie, delFavorite }) {
+const FavoriteMovieList = (props) => {
+  const { movie } = props;
+  // console.log("props", props);
+
+  delFavorite = (movieId) => {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+    axios
+      .delete(
+        `https://intense-shore-03094.herokuapp.com/users/${user}/movies/${movieId}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
+        removeFav(...state, favMovies, response.data);
+        console.log(response);
+
+        alert(
+          `${movie.Title} has been removed from ${user}\'s favorite movie list!`
+        );
+        this.componentDidMount();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <Card className="flex-fill bg-dark text-warning" xs={6} md={6} lg={3}>
       <Card.Body>
@@ -22,9 +50,10 @@ export function FavoriteMovieList({ movie, delFavorite }) {
       </Card.Body>
     </Card>
   );
-}
+};
 
 let mapStateToProps = (state) => {
+  console.log("state", state);
   return {
     movies: state.movies,
     user: state.user,
