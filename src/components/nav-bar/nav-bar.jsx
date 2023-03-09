12c4b0691e54aecd9fa1f6a4325b9082b69a129f
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-03-08 21:21:10 -0400

    nothing

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,41 +1,45 @@
-import React from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { connect } from 'react-redux';
-
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { connect } from "react-redux";
 
 export function Navbar(props) {
-    const { user } = props;
+  const { user } = props;
 
-    handleLogOut = (e) => {
-        e.preventDefault();
-        localStorage.clear();
-        window.open(`/`, '_self');
-        onLoggedOut(user);
-    };
+  handleLogOut = (e) => {
+    e.preventDefault();
+    localStorage.clear();
+    window.open(`/`, "_self");
+    onLoggedOut(user);
+  };
 
-
-    return (
-
-        <Navbar fixed="top" bg="dark" variant="dark">
-            <Navbar.Brand href={`/`} className="text-warning">
-                Martini Shot</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link className="text-primary" href={`/users/${user}`}>Profile</Nav.Link>
-                <Nav.Link size="sm" variant="link" className="float-right text-primary" onClick={(handleLogOut)}>Log out</Nav.Link>
-            </Nav>
-        </Navbar>
-    );
+  return (
+    <Navbar fixed="top" bg="dark" variant="dark">
+      <Navbar.Brand href={`/`} className="text-warning">
+        Martini Shot
+      </Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link className="text-primary" href={`/users/${user}`}>
+          Profile
+        </Nav.Link>
+        <Nav.Link
+          size="sm"
+          variant="link"
+          className="float-right text-primary"
+          onClick={handleLogOut}
+        >
+          Log out
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  );
 }
 
-let mapStateToProps = state => {
+let mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+    user: state.user,
+  };
+};
 
-    return {
-        movies: state.movies,
-        user: state.user,
-
-    };
-}
-
-export default connect(mapStateToProps)
-    (Navbar);
+export default connect(mapStateToProps)(Navbar);
