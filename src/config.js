57efbab91a7e9008603d63891c5cd6f856a06393
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-08-29 13:24:27 -0300

    new api url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const MOVIE_API_URL = "https://movieapi-production-1d07.up.railway.app";
+// const MOVIE_API_URL = "https://movieapi-production-1d07.up.railway.app";
+const MOVIE_API_URL = "https://martini-shot-react.netlify.app"
 
-export { MOVIE_API_URL };
+export { MOVIE_API_URL }
