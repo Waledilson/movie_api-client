3b0ff22a9dd3223fed47235174b7853074a4edf5
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-08-29 14:12:28 -0300

    another new api url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 // const MOVIE_API_URL = "https://movieapi-production-1d07.up.railway.app";
 // const MOVIE_API_URL = "https://martini-shot-react.netlify.app"
-const MOVIE_API_URL = "fancy-kelpie-741dd0.netlify.app"
+// const MOVIE_API_URL = "fancy-kelpie-741dd0.netlify.app"
+const MOVIE_API_URL = "radiant-cassata-b54401.netlify.app"
 export { MOVIE_API_URL }
