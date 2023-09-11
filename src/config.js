6cbf1b57d77bed1ff844433ca1b4ac4410d362bf
waledilson <waledilson@hotmail.com>
2023-09-11 12:23:57 -0300

    this is getting old

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@
 // const MOVIE_API_URL = "https://martini-shot-react.netlify.app";
 // const MOVIE_API_URL = "fancy-kelpie-741dd0.netlify.app"
 // const MOVIE_API_URL = "https://radiant-cassata-b54401.netlify.app";
-const MOVIE_API_URL = "https://main--teal-piroshki-fe95ae.netlify.app/";
+// const MOVIE_API_URL = "https://main--teal-piroshki-fe95ae.netlify.app/";
 // const MOVIE_API_URL = "http://localhost:8080";
+const MOVIE_API_URL = "https://chimerical-tarsier-543164.netlify.app/";
 
 export { MOVIE_API_URL };
