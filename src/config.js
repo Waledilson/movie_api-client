67ab21e09ef8f01468aa0f563b045cc64d5178e7
waledilson <waledilson@hotmail.com>
2023-09-11 10:31:03 -0300

    trying localhost to see if mongodb works

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 // const MOVIE_API_URL = "https://movieapi-production-1d07.up.railway.app";
-const MOVIE_API_URL = "https://martini-shot-react.netlify.app";
+// const MOVIE_API_URL = "https://martini-shot-react.netlify.app";
 // const MOVIE_API_URL = "fancy-kelpie-741dd0.netlify.app"
 // const MOVIE_API_URL = "radiant-cassata-b54401.netlify.app";
 // const MOVIE_API_URL =
 //   "https://64efe4fc59547b0cfa3c6301--martini-shot-react.netlify.app";
+const MOVIE_API_URL = "http://localhost:8080";
 
 export { MOVIE_API_URL };
