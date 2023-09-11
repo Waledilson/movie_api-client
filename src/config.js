d7dec795f9ebd2e406f29318f7502b4fb62face2
waledilson <waledilson@hotmail.com>
2023-09-11 14:43:25 -0300

    newest netlify sacrifice

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
 // const MOVIE_API_URL = "https://movieapi-production-1d07.up.railway.app";
 // const MOVIE_API_URL = "https://martini-shot-react.netlify.app";
-// const MOVIE_API_URL = "fancy-kelpie-741dd0.netlify.app"
-// const MOVIE_API_URL = "https://radiant-cassata-b54401.netlify.app";
-// const MOVIE_API_URL = "https://main--teal-piroshki-fe95ae.netlify.app/";
+const MOVIE_API_URL = "https://martinishot.netlify.app";
 // const MOVIE_API_URL = "http://localhost:8080";
-// const MOVIE_API_URL = "https://chimerical-tarsier-543164.netlify.app";
-// const MOVIE_API_URL = "https://marvelous-sundae-b7591f.netlify.app";
-const MOVIE_API_URL = "https://teal-piroshki-fe95ae.netlify.app";
 
 export { MOVIE_API_URL };
