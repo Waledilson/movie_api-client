1905c6349bce48804af3e637763a4839d7540f75
waledilson <waledilson@hotmail.com>
2023-09-12 08:55:15 -0300

    s

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // const MOVIE_API_URL = "https://movieapi-production-1d07.up.railway.app";
 // const MOVIE_API_URL = "https://martini-shot-react.netlify.app";
 // const MOVIE_API_URL = "https://martinishot.netlify.app";
-const MOVIE_API_URL = "http://localhost:8080";
+const MOVIE_API_URL = "https://localhost:8080";
 
 export { MOVIE_API_URL };
