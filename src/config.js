90a9467498cd1e5a8bfef074ed25979c9472d955
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-11-06 11:38:59 -0400

    trying railway

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-// const MOVIE_API_URL = "https://movieapi-production-1d07.up.railway.app"
+const MOVIE_API_URL = "https://movieapi-production-1d07.up.railway.app"
 // const MOVIE_API_URL = "https://martini-shot-react.netlify.app"
-const MOVIE_API_URL = "https://martinishot.netlify.app"
+// const MOVIE_API_URL = "https://martinishot.netlify.app"
 // const MOVIE_API_URL = "http://localhost:8080";
 // const MOVIE_API_URI = "https://main--martinishot.netlify.app";
 
